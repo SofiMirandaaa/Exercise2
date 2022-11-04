059fa2688a393891d02c5521a6b5eb95c85a064b
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-04 16:17:55 +0700

    add definition and variable array

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -4,7 +4,19 @@ namespace GPA_sort
 {
     class Program
     {
+        //Deklarasi array integer sama dengan 96
+        private int[] a = new int[96];
 
-    }
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int i;
+
+
+
+    }  
+        
+    
 
 }
